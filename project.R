b5227a21ad16aed110fcb4977632353b479f1c89
munsunjuliachoi <82610595+munsunjuliachoi@users.noreply.github.com>
2021-05-02 17:57:29 -0400

    Add files via upload

---
+++ project.R
@@ -0,0 +1,157 @@
+#install.packages("car")
+library(car)
+library(qiime2R)
+#library(ape)
+library(dplyr)
+library(ggplot2)
+library(gplots)
+#library(lme4)
+#library(phangorn)
+#library(phyloseq)
+#library(plotly)
+library(tidyr)
+library(vegan)
+#library(VennDiagram)
+#library(naniar)
+#library(zoo)
+library(tidyverse)
+library(ggpubr)
+#library(rstatix)
+
+##############################################
+#Set UP
+#
+#These are the things that  we need from Qiime:
+#
+#sample-metadata.tsv
+#core-metrics-results/bray_curtis_pcoa_results.qza
+#core-metrics-results/weighted_unifrac_pcoa_results.qza
+#core-metrics-results/rarefied_table.qza
+#rooted-tree.qza
+#taxonomy.qza
+#core-metrics-results/evenness_vector.qza
+#core-metrics-results/faith_pd_vector.qza
+#core-metrics-results/observed_otus_vector.qza
+#core-metrics-results/shannon_vector.qza
+#
+# To get these files you need to scp them from the cluster:
+#
+# first on  your laptop cd to the directory where you want to save them.
+# Then use this code for our example dataset today:
+# mkdir core-metrics-results/
+# scp john2185@bell.rcac.purdue.edu:/depot/microbiome/data/2021_ANSC595/john2185/qiime/moving_pictures_pipeline/* .
+# scp john2185@bell.rcac.purdue.edu:/depot/microbiome/data/2021_ANSC595/john2185/qiime/moving_pictures_pipeline/core-metrics-results/* core-metrics-results/.
+##############################################
+
+
+setwd("~/Desktop/Project/core-metrics-results/")
+#setwd("/Users/john2185/Documents/")
+list.files()
+
+if(!dir.exists("output"))
+  dir.create("output")
+
+metadata<-read_q2metadata("metadata.tsv")
+#metadata2 <- read.delim("sample-metadata.tsv", sep = "\t", header = T, quote = "", stringsAsFactors = F)
+#metadata2 <- metadata2[-1,]
+str(metadata)
+levels(metadata$`treatment`)
+
+
+row.names(metadata) <- metadata[ ,1]
+#metadata <- metadata[,-1]
+row.names(metadata)
+
+#Taxonomy of each OTU
+#tax = read.delim("taxonomy.tsv", header=TRUE, sep="\t")
+
+#tax2 = separate(tax, Taxon, into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";")
+
+##Qiime2r method of reading in the taxonomy files
+#taxonomy<-read_qza("taxonomy.qza")
+#head(taxonomy$data)
+
+#tax2<-parse_taxonomy(taxonomy$data)
+#head(taxonomy)
+
+#This warning means that some cells are empty and that R is replacing empty cells with NA. Now there are other cells that are unclassified that  say, for example `s__` or `g__`. All these need to be removed and replaced with `NA`. 
+
+#All this is OK except that in future use of the taxonomy table, these ASVs will be ignored because they are not classified. Why are ASVs not classified? Its because there is not a close enough match in the database. Just because there is not a good match in the database does not mean they don’t exist, so I wanted to make sure this data was not lost. So in my new code, from lines 200 – 224 I make it so that ASVs that are unclassified at any level are classified as the lowest taxonomic level for which there is a classification.
+#Next, all these `NA` classifications with the last level that was classified
+#tax2 <- taxonomy
+#tax2[] <- t(apply(tax2, 1, zoo::na.locf))
+#row.names(tax2) <- tax2[,1]
+#tax2 = tax2[,-1]
+#tax.clean <- tax2
+
+bc_PCoA<-read_qza("~/Desktop/Project/core-metrics-results/bray_curtis_pcoa_results.qza")
+
+body_colors <- c("Black", "Blue", "Green", "Gray")
+
+bc_meta <- bc_PCoA$data$Vectors %>%
+  select(SampleID, PC1, PC2) %>%
+  inner_join(metadata, by = c("SampleID" = "SampleID"))
+
+my_column <- "treatment"
+#my_column <- "DietTreatment"
+
+ggplot(bc_meta, aes(x=PC1, y=PC2, color=get("treatment"))) +
+  geom_point() + #alpha controls transparency and helps when points are overlapping
+  theme_q2r() +
+  xlab(paste0("PC1 (", round(100*bc_PCoA$data$ProportionExplained[1], digits = 2), "%)")) +
+  ylab(paste0("PC2 (", round(100*bc_PCoA$data$ProportionExplained[2], digits = 2), "%)")) +
+  scale_color_manual(values=body_colors)
+ggsave(paste0("output/BC-ellipse_basic_", my_column,".pdf"), height=2, width=3, device="pdf") # save a PDF 3 inches by 4 inches
+
+
+centroids <- aggregate(cbind(PC1,PC2)~get(my_column),bc_meta,mean)
+colnames(centroids)[1] <- "treatment"
+
+ggplot(bc_meta, aes(x=PC1, y=PC2, color=get(my_column))) +
+  geom_point() + #alpha controls transparency and helps when points are overlapping
+  theme_q2r() +
+  stat_ellipse(level = 0.95, type = "t") +
+  xlab(paste0("PC1 (", round(100*bc_PCoA$data$ProportionExplained[1], digits = 2), "%)")) +
+  ylab(paste0("PC2 (", round(100*bc_PCoA$data$ProportionExplained[2], digits = 2), "%)")) +
+  scale_color_manual(values=body_colors)
+ggsave(paste0("output/BC-ellipse_", my_column,".pdf"), height=2, width=3, device="pdf") # save a PDF 3 inches by 4 inches
+
+ggplot(bc_meta, aes(x=PC1, y=PC2, color=get(my_column))) +
+  geom_point(aes(shape= subject)) + #alpha controls transparency and helps when points are overlapping
+  #geom_point(data=centroids, size = 3) +
+  theme_q2r() +
+  #stat_ellipse(level = 0.95, type = "t") +
+  xlab(paste0("PC1 (", round(100*bc_PCoA$data$ProportionExplained[1], digits = 2), "%)")) +
+  ylab(paste0("PC2 (", round(100*bc_PCoA$data$ProportionExplained[2], digits = 2), "%)")) 
+  #scale_color_manual(values=corn_colors, name = my_column)
+ggsave(paste0("output/BC-ellipse_", my_column,"-subject.pdf"), height=3, width=4.5, device="pdf") # save a PDF 3 inches by 4 inches
+
+##SAME thing but with weighted UniFrac
+
+Wuni_PCoA<-read_qza("~/Desktop/Project/core-metrics-results/weighted_unifrac_pcoa_results.qza")
+
+Wuni_meta <- Wuni_PCoA$data$Vectors %>%
+  select(SampleID, PC1, PC2) %>%
+  inner_join(metadata, by = c("SampleID" = "SampleID"))
+
+centroids <- aggregate(cbind(PC1,PC2)~get(my_column),Wuni_meta,mean)
+
+ggplot(Wuni_meta, aes(x=PC1, y=PC2, color=get(my_column))) +
+  geom_point() + #alpha controls transparency and helps when points are overlapping
+  #geom_point(data=centroids, size = 3) +
+  theme_q2r() +
+  stat_ellipse(level = 0.95, type = "t") +
+  xlab(paste0("PC1 (", round(100*Wuni_PCoA$data$ProportionExplained[1], digits = 2), "%)")) +
+  ylab(paste0("PC2 (", round(100*Wuni_PCoA$data$ProportionExplained[2], digits = 2), "%)")) +
+  scale_color_manual(values=body_colors)
+ggsave(paste0("output/Wuni-ellipse_", my_column,".pdf"), height=2, width=3, device="pdf") # save a PDF 3 inches by 4 inches
+
+ggplot(Wuni_meta, aes(x=PC1, y=PC2, color=get(my_column))) +
+  geom_point(aes(shape= subject)) + #alpha controls transparency and helps when points are overlapping
+  #geom_point(data=centroids, size = 3) +
+  theme_q2r() +
+  stat_ellipse(level = 0.95, type = "t") +
+  xlab(paste0("PC1 (", round(100*Wuni_PCoA$data$ProportionExplained[1], digits = 2), "%)")) +
+  ylab(paste0("PC2 (", round(100*Wuni_PCoA$data$ProportionExplained[2], digits = 2), "%)")) +
+  scale_color_manual(values=body_colors, name = "Body Site")
+ggsave(paste0("output/Wuni-ellipse_", my_column,"-subject.pdf"), height=3, width=4.5, device="pdf") # save a PDF 3 inches by 4 inches
